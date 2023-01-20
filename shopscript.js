ed563ba142d3bd2a077c053c942cb8180bb24875
dlirA01 ♡ <arild.edvin.andreassen@gmail.com>
2023-01-20 14:57:34 +0100

    Fixed cart stuff

--- shopscript.js
+++ shopscript.js
@@ -8,6 +8,9 @@ window.addEventListener("load", function() {
 
 
 let cart = [];
+let index = 0;
+let cartMenu = document.getElementsByClassName("cart-menu")[0];
+let cartMenuNote;
 
 function addToCart(title, price) {
     cart.push({productTitle: title, productPrice: price, productQuantity: 1});
@@ -18,22 +21,40 @@ function addToCart(title, price) {
 
 function renderCart() {
     let listHTML = "";
+    cartMenu.classList.remove("open");
 
     console.log(parseInt(document.getElementsByClassName("cart-menu-total-numb")[0].innerHTML));
 
-    let prodPrice;
-    cart.map(prod => listHTML+= `<li class="cart-menu-item flexbox">
-                    <p class="cart-menu-text"><span>${prod.productTitle}</span><span>$${prod.productPrice}</span><span>1x</span><span class="cart-remove flexbox"><svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 323.37 323.37"><defs><style>.cls-1{fill:#fff;stroke:#fff;stroke-linecap:round;stroke-miterlimit:10;stroke-width:2.5em;}</style></defs><line class="cls-1" x1="15" y1="15" x2="308.37" y2="308.37"/><line class="cls-1" x1="15" y1="308.37" x2="308.37" y2="15"/></svg></span></p>
-                </li>`);
-    cart.map(prod => prodPrice = prod.productPrice);
-    document.getElementsByClassName("cart-menu-total-numb")[0].innerHTML = parseInt(document.getElementsByClassName("cart-menu-total-numb")[0].innerHTML) + prodPrice
+    let prodPrice = 0;
+    cart.map((prod, index) => {
+        listHTML+= `<li id="ci-${index}" class="cart-menu-item flexbox">
+                    <p class="cart-menu-text"><span>${prod.productTitle}</span><span>$${prod.productPrice}</span><span>1</span><button class="cart-remove flexbox" onClick="removeItem(${index})"><svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 323.37 323.37"><defs><style>.cls-1{fill:#fff;stroke:#fff;stroke-linecap:round;stroke-miterlimit:10;stroke-width:2.5em;}</style></defs><line class="cls-1" x1="15" y1="15" x2="308.37" y2="308.37"/><line class="cls-1" x1="15" y1="308.37" x2="308.37" y2="15"/></svg></button></p>
+                </li>`
+        prodPrice = prodPrice + Number(prod.productPrice)
+    });
+    document.getElementsByClassName("cart-menu-total-numb")[0].innerHTML = prodPrice.toFixed(2);
 
     document.getElementsByClassName("cart-menu-items")[0].innerHTML = listHTML;
     document.getElementsByClassName("cart-length")[0].innerHTML = cart.length;
     document.getElementsByClassName("navbar-button-note")[0].innerHTML = cart.length;
+
+    cartMenu.style.opacity = "1";
+    cartMenu.style.pointerEvents = "auto";
+    cartMenu.classList.add("open");
 }
 
-let cartMenu = document.getElementsByClassName("cart-menu")[0];
+function removeItem(index) {
+    document.getElementById("ci-"+index).style.display = "none";
+    cart.splice(index, 1);
+
+    renderCart();
+    if (document.getElementsByClassName("cart-menu-items")[0].childNodes.length === 0) {
+        cartMenuNote = document.createElement("p");
+        cartMenuNote.classList.add("cart-menu-note");
+        cartMenuNote.innerHTML = "Your cart is empty";
+        document.getElementsByClassName("cart-menu-items")[0].appendChild(cartMenuNote);
+    }
+}
 
 document.getElementsByClassName("navbar-item-last")[0].addEventListener("click", function() {
 
